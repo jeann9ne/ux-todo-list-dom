f1c60ed1326eaa847cd252f1e9ea7ca8cb42519f
jeann9ne <jeanninecoons@gmail.com>
2020-05-05 15:41:16 -0400

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 
 let todos = [
     {
-        completed: false,
-        description: "Take Jim to the hair salon"
+      completed: true,
+      description: "Take Jim to the hair salon",
     },
     {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
+      completed: true,
+      description: "Drop off wedding invitation at mailbox",
     },
     {
-        completed: false,
-        description: "Pick up the cake"
+      completed: false,
+      description: "Pick up the cake",
     },
     {
-        completed: false,
-        description: "Call the caterers"
-    }
-];
-
-function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
-
-    h1.innerText = "Todo List"
-
-    app.appendChild(h1)
-    // and maybe some here
-}
-
-renderTodoApp()
+      completed: false,
+      description: "Call the caterers",
+    },
+  ];
+  
+  function addToDo() {
+    let val = document.getElementById("todo-input").value;
+    let newObj = {completed:false, description: val};
+    todo = todos.unshift(newObj);
+  
+    renderTodoApp();
+  }
+  
+  function renderTodoApp() {
+    let items = "";
+    let app = document.querySelector("#app");
+    todos.forEach(function (todo) {
+      items += `<li class="items"><input class="check" ${
+        todo.completed ? "checked='checked'" : ""
+      } type="checkbox">${todo.description}</li>`;
+    });
+  
+    app.innerHTML = `<h1> Todo List </h2>
+      <ul class="list" id="items>${items}</ul>`;
+  }
+  
+  renderTodoApp();
